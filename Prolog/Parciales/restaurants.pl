30ab8e4564e08161218fa931abc3d03b9720305c
GonGarciaFontenla <ggarciafontenla@frba.utn.edu.ar>
2024-08-09 15:20:31 -0300

    Resolucion parcial [200~Restaurantes y vinos

--- Prolog/Parciales/restaurants.pl
+++ Prolog/Parciales/restaurants.pl
@@ -7,6 +7,9 @@ menu(panchoMayo, carta(200, hamburguesa)).
 menu(finoli, carta(2000, hamburguesa)).
 menu(finoli, pasos(15, 15000, [chateauMessi, francescoliSangiovese, susanaBalboaMalbec], 6)).
 menu(noTanFinoli, pasos(2, 3000, [guinoPin, juanaDama],3)).
+%Nuevo tipo de menu --> Punto 6
+%menu(Restaurante, experiencia(cantPlatos, cantDias, precioPorDia, vinos, comensales)). 
+menu(gonza, experiencia(10, 3, 20000, [chateauMessi, francescoliSangiovese, juanaDama], 5)).
 
 vino( chateauMessi, francia, 5000).
 vino( francescoliSangiovese, italia, 1000).
@@ -16,13 +19,14 @@ vino( guinoPin, argentina, 500).
 vino( juanaDama, argentina, 1000).
 
 %Cuáles son los restaurantes de más de N estrellas por barrio.
-superaNestrellas(Restaurantes, N, Barrio) :-
-    restaurante(_, _, Barrio), %Inversibilidad
-    findall(Restaurante, (restaurante(Restaurante, Estrellas, Barrio), Estrellas > N), Restaurantes). 
+superaNestrellas(Restaurante, N, Barrio) :-
+    restaurante(Restaurante, Estrellas, Barrio), %Inversibilidad
+    Estrellas > N. 
 
 %Cuáles son los restaurantes sin estrellas.
-sinEstrellas(Restaurantes) :-
-    findall(Restaurante, (menu(Restaurante, _), not(restaurante(Restaurante, _,_))), Restaurantes).
+sinEstrellas(Restaurante) :-
+    menu(Restaurante, _), 
+    not(restaurante(Restaurante, _,_)). 
 
 %Si un restaurante está mal organizado, que es cuando tiene algún menú que tiene más pasos que la cantidad
 %de vinos disponibles o cuando tiene en su menú a la carta dos veces una misma comida con diferente precio.
@@ -69,13 +73,17 @@ calcularPrecioPromedio(Restaurante, PrecioTotal):-
     calcularPrecioVinos(Vinos, PrecioVino), 
     PrecioTotal is (Precio + PrecioVino) / Comensales. 
 
+calcularPrecioPromedio(Restaurante, PrecioTotal):-
+    menu(Restaurante, experiencia(_,CantDias, PrecioPorDia, Vinos, Comensales)),
+    calcularPrecioVinos(Vinos, PrecioVino), 
+    PrecioTotal is (PrecioPorDia + PrecioVino) / Comensales * CantDias.
+
 calcularPrecioVinos(Vinos, PrecioTotalVinos):-
     findall(Precio, (member(Vino,Vinos), calcularPrecioVino(Vino, Precio)), PrecioVinos), 
     sumlist(PrecioVinos, PrecioTotalVinos). 
 
 calcularPrecioVino(Vino, PrecioVino):-
-    vino(Vino, argentina, Precio),  
-    PrecioVino is Precio.
+    vino(Vino, argentina, PrecioVino).
 
 calcularPrecioVino(Vino, PrecioVino):-
     vino(Vino, PaisProduccion, Precio), 
